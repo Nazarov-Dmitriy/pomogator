51143ec78b130831294f3d76992f1dd4f75f7926
Пользователь <olegator10205@gmail.com>
2024-06-25 14:49:44 +0300

    fixed about component

--- src/components/main/AboutComponent.vue
+++ src/components/main/AboutComponent.vue
@@ -1,45 +1,4 @@
-<script setup>
-import { ref, onMounted, onUnmounted, watch } from 'vue'
-import IconExit from '../icons/AboutComponent/IconExit.vue';
-import IconQuestion from '../icons/AboutComponent/IconQuestion.vue';
-import IconSheet from '../icons/AboutComponent/IconSheet.vue';
-import BackgroundSvg from '../icons/AboutComponent/svg/BackgroundSvg.vue';
-
-const cardsInfo = ref([
-  {
-    icon: IconSheet,
-    description: 'Использование цифровых технологий в обучении'
-  },
-  {
-    icon: IconQuestion,
-    description: 'Возможность задавать вопросы лектору во время эфира'
-  },
-  {
-    icon: IconExit,
-    description: 'Все зарегистрированные пользователи получают сертификат'
-  }
-])
-
-const visibleCards = ref(false)
-const updateScreenWidth = () => {
-  if (window.innerWidth < 768) {
-    visibleCards.value = true
-    console.log(visibleCards.value)
-  } else {
-    visibleCards.value = false
-    console.log(visibleCards.value)
-  }
-}
-
-onMounted(() => {
-  window.addEventListener('resize', updateScreenWidth)
-  updateScreenWidth()
-})
-
-onUnmounted(() => {
-  window.removeEventListener('resize', updateScreenWidth)
-})
-</script>
+<script setup></script>
 
 <template>
   <section class="webinar">
@@ -54,160 +13,213 @@ onUnmounted(() => {
         </div>
         <div class="webinar__info">
           <h2 class="webinar__info-title">Вебинары онлайн</h2>
-          <p v-show="!visibleCards" class="webinar__info-subtitle">
-            Для арегистрированных пользователей есть уникальная возможность присутствовать на
-            практических онлайн вебинарах с получением сертификата
-          </p>
-          <div v-show="!visibleCards" class="webinar__info-cards">
-            <div v-for="(card, index) in cardsInfo" :key="index" class="webinar__info-card">
-              <div class="webinar__info-card-icon">
-                <component :is="card.icon" />
+          <div class="webinar__info-wrap">
+            <p class="webinar__info-subtitle">
+              Для арегистрированных пользователей есть уникальная возможность присутствовать на
+              практических онлайн вебинарах с получением сертификата
+            </p>
+            <div class="webinar__info-cards">
+              <div class="webinar__info-card">
+                <div class="webinar__info-card-icon">
+                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <rect x="6.33334" y="1" width="8.66667" height="8.66667" stroke="white" stroke-width="2" />
+  <rect x="6.33334" y="9" width="8.66667" height="8.66667" stroke="white" stroke-width="2" />
+  <rect x="14.3333" y="1" width="8.66667" height="8.66667" stroke="white" stroke-width="2" />
+  <rect x="14.3333" y="9" width="8.66667" height="8.66667" stroke="white" stroke-width="2" />
+  <rect x="14.3333" y="17" width="8.66667" height="8.66667" stroke="white" stroke-width="2" />
+  <rect x="1" y="22.3333" width="8.66667" height="8.66667" stroke="white" stroke-width="2" />
+  <rect x="22.3333" y="1" width="8.66667" height="8.66667" stroke="white" stroke-width="2" />
+  <rect x="22.3333" y="9" width="8.66667" height="8.66667" stroke="white" stroke-width="2" />
+  <rect x="22.3333" y="17" width="8.66667" height="8.66667" stroke="white" stroke-width="2" />
+</svg>
+                </div>
+                <p class="webinar__info-card-subtitle">
+                  Использование цифровых технологий в обучении
+                </p>
               </div>
-              <p class="webinar__info-card-subtitle">{{ card.description }}</p>
+              <div class="webinar__info-card">
+                <div class="webinar__info-card-icon">
+                  <svg
+                    width="32"
+                    height="32"
+                    viewBox="0 0 32 32"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M16 29.3333C23.3638 29.3333 29.3333 23.3638 29.3333 16C29.3333 8.63621 23.3638 2.66667 16 2.66667C8.63619 2.66667 2.66666 8.63621 2.66666 16C2.66666 17.8078 3.02644 19.5316 3.6783 21.1036C4.01487 21.9153 4.4293 22.6865 4.91228 23.4079C5.07437 23.6501 4.77028 25.1807 3.99999 28C6.81926 27.2297 8.34992 26.9256 8.59206 27.0877C9.31352 27.5707 10.0847 27.9851 10.8964 28.3217C12.4684 28.9735 14.1922 29.3333 16 29.3333Z"
+                      stroke="white"
+                      stroke-width="3"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M19.0524 16.2359C18.4374 16.765 17.9453 17.3012 17.5763 17.8445C17.221 18.3878 17.0433 18.9812 17.0433 19.6247H14.9317C14.9317 18.7954 15.1298 18.0161 15.5262 17.2869C15.9226 16.5576 16.4829 15.8713 17.2073 15.2279C17.7267 14.756 18.1162 14.3271 18.3759 13.941C18.6492 13.555 18.7859 13.0331 18.7859 12.3753C18.7859 11.7462 18.5535 11.2172 18.0888 10.7882C17.6241 10.345 17.0228 10.1233 16.2847 10.1233C15.6424 10.1233 15.0957 10.2592 14.6446 10.5308C14.1936 10.7882 13.8109 11.1528 13.4966 11.6247L12 10.1662C12.451 9.49419 13.0524 8.96515 13.8041 8.57909C14.5558 8.19303 15.4032 8 16.3462 8C17.2893 8 18.1093 8.19303 18.8064 8.57909C19.5034 8.95085 20.0433 9.45845 20.426 10.1019C20.8087 10.7453 21 11.4531 21 12.2252C21 13.1403 20.8428 13.8981 20.5285 14.4987C20.2141 15.0992 19.7221 15.6783 19.0524 16.2359ZM14.6446 24V21.2547H17.3303V24H14.6446Z"
+                      fill="white"
+                    />
+                  </svg>
+                </div>
+                <p class="webinar__info-card-subtitle">
+                  Возможность задавать вопросы лектору во время эфира
+                </p>
+              </div>
+              <div class="webinar__info-card">
+                <div class="webinar__info-card-icon">
+                  <svg
+                    width="32"
+                    height="32"
+                    viewBox="0 0 33 32"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M16.6611 4H4.66663V28H16.6666"
+                      stroke="white"
+                      stroke-width="3"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M17.3333 22L11.3333 16L17.3333 10"
+                      stroke="white"
+                      stroke-width="3"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M28.6666 15.9945H11.3333"
+                      stroke="white"
+                      stroke-width="3"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </div>
+                <p class="webinar__info-card-subtitle">
+                  Все зарегистрированные пользователи получают сертификат
+                </p>
+              </div>
+            </div>
+            <div class="webinar__info-button-wrap">
+              <button class="webinar__info-button info-button">Регистрация</button>
             </div>
           </div>
-          <div v-show="!visibleCards" class="webinar__info-button-wrap">
-            <button class="webinar__info-button info-button">Регистрация</button>
-          </div>
         </div>
       </div>
+    </div>
 
-      <div v-show="visibleCards" class="webinar__info">
-        <p class="webinar__info-subtitle">
-          Для зарегистрированных пользователей есть уникальная возможность присутствовать на
-          практических онлайн вебинарах с получением сертификата
-        </p>
-      </div>
-      <div v-show="visibleCards" class="webinar__info-cards">
-        <div v-for="(card, index) in cardsInfo" :key="index" class="webinar__info-card">
-          <div class="webinar__info-card-icon">
-            <component :is="card.icon" />
-          </div>
-          <p class="webinar__info-card-subtitle">{{ card.description }}</p>
-        </div>
-      </div>
-      <div v-show="visibleCards" class="webinar__info-button-wrap">
-        <button class="webinar__info-button info-button">Регистрация</button>
-      </div>
-    </div>
-    <div class="webinar__background-svg">
-     <BackgroundSvg/>
-    </div>
+    <div class="webinar__background-svg"></div>
   </section>
 </template>
 
-<style lang="scss">
+<style scoped lang="scss">
 @import '@/assets/styles/_variables.scss';
 
 .webinar {
   border: 2px solid #f84343;
-  padding: 60px 80px;
-  background: $blue;
-  position: relative;
-  overflow: hidden;
+  padding: 60px 30px;
+  background: #5b94ea;
+  box-sizing: border-box;
 
-  @media (max-width: 950px) {
-    padding: 60px 30px;
-  }
-  @media (max-width: 768px) {
+  @media (max-width: 991px) {
     padding: 40px;
   }
 
-  @media (max-width: 378px) {
+  @media (max-width: 576px) {
     padding: 32px 16px;
-  }
-
-  @media (max-width: 640px) {
-    padding: 20px;
-  }
-
-  @media (max-width: 420px) {
-    padding: 20px 15px;
   }
 }
 
 .webinar__container {
-  max-width: 1440px;
+  max-width: 1400px;
   margin: 0 auto;
-  box-sizing: border-box;
 
-  @media (max-width: 1440) {
-    max-width: 1200px;
+  @media (max-width: 991px) {
+    max-width: 673px;
   }
 
-  @media (max-width: 378px) {
-    max-width: 320px;
+  @media (max-width: 576px) {
+    max-width: 287px;
   }
 }
 
 .webinar__wrapper {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  display: flex;
+  justify-content: space-between;
   align-items: center;
-
   gap: 38px;
-  width: 100%;
   position: relative;
 
-  @media (max-width: 1400px) {
-    gap: 10px;
-  }
-
-  @media (max-width: 768px) {
-    align-items: flex-start;
-    margin-bottom: 10px;
+  @media (max-width: 991px) {
+    display: block;
   }
 }
 
 .webinar__hero {
   width: 100%;
+
 }
 
 .webinar__hero-img {
   width: 100%;
 
-  @media (max-width: 900px) {
-    max-width: 500px;
+  @media (max-width: 991px) {
+    position: absolute;
+    width: 212px;
   }
 
-  @media (max-width: 768px) {
-    max-width: 100%;
+  @media (max-width: 576px) {
+    width: 131px;
+    top: -15px;
+    left: -15px;
   }
 }
-
+.webinar__info {
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+}
 .webinar__info-title {
+  font-family: var(--second-family);
+  font-weight: 600;
   font-size: 48px;
   line-height: 117%;
   text-align: center;
-  color: $black;
-  margin-bottom: 32px;
+  color: #060c1f;
 
-  @media (max-width: 1100px) {
-    font-size: 32px;
+  @media (max-width: 991px) {
+    text-align: left;
+    font-size: 36px;
+    line-height: 120%;
+    margin-left: 230px;
   }
 
-  @media (max-width: 768px) {
-    font-size: 36px;
-    line-height: 117%;
+  @media (max-width: 576px) {
+    margin-left: 130px;
+    font-size: 32px;
   }
 }
 
+.webinar__info-wrap {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  
+}
+
 .webinar__info-subtitle {
+  font-family: var(--font-family);
+  font-weight: 500;
   font-size: 24px;
   line-height: 133%;
-  color: $white;
-  margin-bottom: 24px;
-  width: 738px;
-  width: 100%;
+  color: #fff;
 
-  @media (max-width: 1100px) {
+  @media (max-width: 991px) {
     font-size: 20px;
+    margin-left: 230px;
   }
 
-  @media (max-width: 768px) {
-    width: 100%;
-    font-size: 20px;
-    line-height: 120%;
+  @media (max-width: 576px) {
+    margin: 0;
     text-align: center;
   }
 }
@@ -216,74 +228,52 @@ onUnmounted(() => {
   grid-template-columns: repeat(3, 1fr);
   gap: 20px;
 
-  @media (max-width: 400px) {
-    grid-template-columns: repeat(1, 1fr);
-    gap: 10px;
-    margin-bottom: 24px;
+  @media (max-width: 576px) {
+    grid-template-columns: 1fr;
   }
 }
-
 .webinar__info-card {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   gap: 10px;
-  margin-bottom: 24px;
 }
-
+.webinar__info-card-icon {
+}
 .webinar__info-card-subtitle {
+  font-family: var(--font-family);
   font-weight: 400;
   font-size: 16px;
   line-height: 150%;
   text-align: center;
-  color: $white;
+  color: #fff;
 
-  @media (max-width: 1100px) {
-    font-size: 14px;
-  }
-
-  @media (max-width: 768px) {
+  @media (max-width: 991px) {
     font-size: 14px;
     line-height: 143%;
   }
 }
-
 .webinar__info-button-wrap {
   display: flex;
   justify-content: flex-end;
-  align-items: center;
-  position: relative;
-  z-index: 5;
-  @media (max-width: 378px) {
-    justify-content: center;
-  }
 }
-
-.info-button {
-  border-radius: 32px;
-  padding: 12px 24px;
-  width: 152px;
-  height: 48px;
-  background: $primary-red;
-  border: none;
-  font-family: var(--font-family);
-  font-weight: 500;
-  font-size: 16px;
+.webinar__info-button {
   line-height: 150%;
-  color: $white;
+  color: #fff;
+  padding: 12px 24px;
+  background: #f84343;
+  border: none;
+  border-radius: 32px;
   cursor: pointer;
-  text-align: center;
 
-  @media (max-width: 400px) {
+  @media (max-width: 576px) {
     width: 100%;
+    text-align: center;
   }
 }
-
+.info-button {
+}
 .webinar__background-svg {
-  position: absolute;
-  bottom: 0;
-  right: 0;
-  z-index: 2;
 }
 </style>
