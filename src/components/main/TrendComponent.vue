d98c18b906e51faf0caf122fb42feacb763a4bcc
Krestoparv <krestoprav@mail.ru>
2024-06-26 00:13:31 +0500

    add верстка Направления

--- src/components/main/TrendComponent.vue
+++ src/components/main/TrendComponent.vue
@@ -1,11 +1,71 @@
 <template>
     <div>
-        Направления        
+        <HeaderComponent />
     </div>
+
+    <!-- <div>
+        Направления        
+    </div> -->
+
+    <section class="trend-container">
+        
+        <div id="app">
+            <div v-for="(item, index) in items" v-bind:key="index">
+            <p>{{ item.name }}</p>
+            </div>
+        </div>
+
+        
+        <!-- <ul>
+        <li>
+        <button type="button" @click="onClickFirstPage">First</button>
+        </li>
+        <li>
+        <button type="button" @click="onClickPreviousPage">Previous</button>
+        </li>
+        <li v-for="page in pages" :key="page.id">
+        <button type="button" @click="onClickPage(page.id)">
+        {{ page.title }}
+        </button>
+        </li>
+        <li>
+        <button type="button" @click="onClickNextPage">Next</button>
+        </li>
+        <li>
+        <button type="button" @click="onClickLastPage">Last</button>
+        </li>
+        </ul> -->
+
+
+        <div>
+            <FooterComponent />
+        </div>
+    </section>
 </template>
 <script setup>
+import HeaderComponent from '@/components/main/HeaderComponent.vue'
+import FooterComponent from '@/components/main/FooterComponent.vue'
 
 </script>
+
+<script>
+
+export default {
+    data() {
+        return {
+            items: [
+            { name: 'Иван', age: 30 }, 
+            { name: 'Мария', age: 25 },
+            { name: 'Мария', age: 25 }
+            ]
+        }
+    }
+}
+</script>
+
 <style lang="scss">
-    
+.trend-container {
+    width: 100%;
+    background-color: aqua
+}    
 </style>
