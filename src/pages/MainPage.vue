53a2f170c4db8d59c20b651339dd59c40be84a17
Nazarov-Dmitriy <Difat1@inbox.ru>
2024-06-27 17:17:07 +0300

    about add

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="main-page">
         <HeaderComponent />
+        <AboutComponent />
     </div>
 </template>
 <script setup>
 import HeaderComponent from '@/components/main/HeaderComponent.vue'
+import AboutComponent from '../components/main/AboutComponent.vue';
+
 </script>
 <style scoped lang="scss">
 .main-page {
