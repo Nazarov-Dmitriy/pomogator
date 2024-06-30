074b74253ea69f2f9e9dc4de2cb8a98908ea3c86
Krestoparv <krestoprav@mail.ru>
2024-06-30 22:24:19 +0500

    add_trends

--- src/components/main/TrendComponent.vue
+++ src/components/main/TrendComponent.vue
@@ -1,170 +1,208 @@
 <template>
-    <HeaderComponent />
-
-    <!-- <div>
-        Направления        
-    </div> -->
-
     <section class="trends__contaner">
         <div class="trends">
-            <p>Содержание</p>
+            <h2 class="trends__header">
+                Направления
+            </h2>
             <div class="trends__list">
-                <div class="trends__card" v-for="(trend, index) in trends" v-bind:key="index">
-                    <img class="trends__card__img" :src=trend.imageSrc alt="trends__card IMG">
-                    <div class="trends__card_info">
-                        <div class="trends__card__hashtags">
-                            <p>{{ trend.firstHashtag }}</p>
-                            <p>{{ trend.secondHashtag }}</p>
-                        </div>
-                        <p class="trends__card__name">{{ trend.name }}</p>
-                        <div class="trends__card_statistic">
-                            <div class="trends__card__likes">
-                                <img src="@/assets/icons/trends/like.svg" alt="">
-                                <p>{{ trend.likes }}</p>
-                            </div>
-                            <div class="trends__card__view">
-                                <img src="@/assets/icons/trends/eye.svg" alt="">
-                                <p>{{ trend.view }}</p>
-                            </div>
-                            <p>Дата публикации {{ trend.publicationData }}</p>
-                        </div>
+                <div class="trends__card">
+                    <div class="trends__card-name">
+                        Химия
+                    </div>
+                    <div class="trends__card-info">
+                        <ul>
+                            <li>Виртуальные практикумы и лабораторные работы</li>
+                            <li>Демонстрационные эксперименты</li>
+                            <li>Моделирование химических процессов</li>
+                            <li>Проекты и исследования</li>
+                        </ul>
+                        <BtnGradient class="btn__trends-card">
+                            Подробнее
+                        </BtnGradient>
+                    </div>
+                </div>
+                <div class="trends__card">
+                    <div class="trends__card-name">
+                        Физика
+                    </div>
+                    <div class="trends__card-info">
+                        <ul>
+                            <li>Виртуальные практикумы и лабораторные работы</li>
+                            <li>Демонстрационные эксперименты</li>
+                            <li>Электронные таблицы и графики</li>
+                            <li>Наглядное представление результатов экспериментов</li>
+                        </ul>
+                        <BtnGradient class="btn__trends-card">
+                            Подробнее
+                        </BtnGradient>
+                    </div>
+                </div>
+                <div class="trends__card">
+                    <div class="trends__card-name">
+                        Биология
+                    </div>
+                    <div class="trends__card-info">
+                        <ul>
+                            <li>Виртуальные практикумы и лабораторные работы</li>
+                            <li>Демонстрационные эксперименты</li>
+                            <li>Предметные тестирования и диагностики</li>
+                            <li>Проекты и исследования</li>
+                        </ul>
+                        <BtnGradient class="btn__trends-card">
+                            Подробнее
+                        </BtnGradient>
+                    </div>
+                </div>
+                <div class="trends__card">
+                    <div class="trends__card-name">
+                        Робототехника
+                    </div>
+                    <div class="trends__card-info">
+                        <ul>
+                            <li>Конструирование и программирование роботов</li>
+                            <li>VR-очки в проектах и исследованиях</li>
+                            <li>3D-моделирование</li>
+                            <li>Научно-техническое творчество</li>
+                        </ul>
+                        <BtnGradient class="btn__trends-card">
+                            Подробнее
+                        </BtnGradient>
                     </div>
                 </div>
             </div>
+            <div class="trends__proposal">
+                Если не нашли на нашем сайте нужный материал, предложите свою тему по направлению или задайте вопрос
+            </div>
+            <BtnGradient class="btn__proposal">
+                Предложить материал
+            </BtnGradient>
         </div>
     </section>
-    <FooterComponent />
 </template>
 
 <script setup>
-import HeaderComponent from '@/components/main/HeaderComponent.vue'
-import FooterComponent from '@/components/main/FooterComponent.vue'
-import flask_big_svg from "@/assets/icons/trends/flask-big.svg"
-import tube_svg from "@/assets/icons/trends/tube.svg"
-import lattice_svg from "@/assets/icons/trends/lattice.svg" 
-import electron_svg from "@/assets/icons/trends/electron.svg" 
-
+import BtnGradient from '../btns/BtnGradient.vue';
 </script>
-
 <script>
-
-export default {
-    data () {
-        return {
-            trends: [
-                { name: 'Роль информационных технологий в науке', 
-                    imageSrc: flask_big_svg, firstHashtag: '#статья', 
-                    secondHashtag: '#химия', publicationData: '01.05.2024',
-                    likes: 100, view: 100}, 
-                { name: 'IT-технологии в физике: новые горизонты исследований', 
-                    imageSrc: tube_svg, firstHashtag: '#статья', 
-                    secondHashtag: '#физика', publicationData: '01.05.2024',
-                    likes: 100, view: 100}, 
-                { name: 'Новые IT технологии в химии', 
-                    imageSrc: lattice_svg, firstHashtag: '#статья', 
-                    secondHashtag: '#химия', publicationData: '01.05.2024',
-                    likes: 100, view: 100}, 
-                { name: 'Робототехника: будущее уже здесь', 
-                    imageSrc: electron_svg, firstHashtag: '#статья', 
-                    secondHashtag: '#робототехника', publicationData: '01.05.2024',
-                    likes: 100, view: 100}, 
-                { name: 'Роль информационных технологий в науке', 
-                    imageSrc: flask_big_svg, firstHashtag: '#статья', 
-                    secondHashtag: '#химия', publicationData: '01.05.2024',
-                    likes: 100, view: 100}, 
-                { name: 'IT-технологии в физике: новые горизонты исследований', 
-                    imageSrc: tube_svg, firstHashtag: '#статья', 
-                    secondHashtag: '#физика', publicationData: '01.05.2024',
-                    likes: 100, view: 100}, 
-                { name: 'Новые IT технологии в химии', 
-                    imageSrc: lattice_svg, firstHashtag: '#статья', 
-                    secondHashtag: '#химия', publicationData: '01.05.2024',
-                    likes: 100, view: 100}, 
-                { name: 'Робототехника: будущее уже здесь', 
-                    imageSrc: electron_svg, firstHashtag: '#статья', 
-                    secondHashtag: '#робототехника', publicationData: '01.05.2024',
-                    likes: 100, view: 100},
-                { name: 'Роль информационных технологий в науке', 
-                    imageSrc: flask_big_svg, firstHashtag: '#статья', 
-                    secondHashtag: '#химия', publicationData: '01.05.2024',
-                    likes: 100, view: 100}, 
-                { name: 'IT-технологии в физике: новые горизонты исследований', 
-                    imageSrc: tube_svg, firstHashtag: '#статья', 
-                    secondHashtag: '#физика', publicationData: '01.05.2024',
-                    likes: 100, view: 100}, 
-                { name: 'Новые IT технологии в химии', 
-                    imageSrc: lattice_svg, firstHashtag: '#статья', 
-                    secondHashtag: '#химия', publicationData: '01.05.2024',
-                    likes: 100, view: 100}, 
-                { name: 'Робототехника: будущее уже здесь', 
-                    imageSrc: electron_svg, firstHashtag: '#статья', 
-                    secondHashtag: '#робототехника', publicationData: '01.05.2024',
-                    likes: 100, view: 100}, 
-            ]
-        }
-    }
-}
 </script>
 
 <style lang="scss">
 .trend-container {
     width: 100%;
-}    
+}
 
 .trends {
     max-width: 1440px;
     margin: 0 auto;
-    padding: 20px 80px 14px 80px;
+    padding: 60px 80px 60px 80px;
+
+    @media (max-width: $lg) {
+        padding: 40px 40px 40px 40px;
+    }
+
+    @media (max-width: $sm) {
+        padding: 32px 16px 32px 16px;
+    }
+}
+
+.trends__header{
+    font-weight: 600;
+    font-size: 36px;
+    line-height: 43px;
+    color: $blue-primary;
+
+    @media (max-width: $lg) {
+        font-size: 32px;
+        line-height: 40px;
+    }
 }
 
 .trends__list {
     display: grid;
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: repeat(4, 1fr);
     grid-gap: 16px;
+    padding: 32px 0 32px 0;
+
+    @media (max-width: $xl) {
+        grid-template-columns: repeat(2, 1fr);
+    }
+
+    @media (max-width: $sm) {
+        grid-template-columns: repeat(1, 1fr);
+    }
 }
 
 .trends__card {
-    padding-bottom: 16px;
     border: 2px solid $blue;
     border-radius: 32px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between; 
 }
 
-.trends__card__img {
+.trends__card-name {
+    background-color: $blue;
+    border-radius: 28px 28px 0 0;
+    padding: 16px 0 16px 0;
+    text-align: center;
+    color: $white;
+    font-weight: 500;
+    font-size: 24px;
+
+    @media (max-width: $xl) {
+        font-size: 20px;
+        line-height: 24px;
+    }
+}
+
+.trends__card-info {
+    padding: 16px 16px 16px 16px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between; 
+    background: $gradient-background;
+    border-radius: 0 0 32px 32px;
+    height: 100%;
+}
+
+ul {
+    padding-bottom: 10px;
+    padding-left: 20px;
+}
+
+li {
+    list-style-type: disc;
     width: 100%;
-    object-fit: cover;
-    border-radius: 32px 32px 0 0
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 24px;
+    
+    @media (max-width: $xl) {
+        font-size: 14px;
+        line-height: 20px;
+    }
 }
 
-.trends__card_info{
-    padding: 16px;
-}
-
-.trends__card__hashtags{
+.btn__trends-card {
     width: 100%;
-    display: flex;
-    justify-content: flex-end;
-    gap: 40px;
 }
 
-.trends__card__name {
-    width: 100%;
-    padding-top: 7px;
-    padding-bottom: 20px;
+.trends__proposal{
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 24px;
+    color: $blue;
+    padding-bottom: 16px;
+
+    @media (max-width: $xl) {
+        font-weight: 400;
+        font-size: 14px;
+        line-height: 20px;
+    }
 }
 
-.trends__card_statistic {
-    display: flex;
-    justify-content: space-between;
+.btn__proposal {
+    width: 230px;
+    padding: 0;
 }
-
-.trends__card__likes {
-    display: flex;
-    justify-content: space-between;
-}
-
-.trends__card__view {
-    display: flex;
-    justify-content: space-between;
-}
-
 </style>
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -2,11 +2,13 @@
     <div class="main-page">
         <HeaderComponent />
         <AboutComponent />
+        <TrendComponent />
     </div>
 </template>
 <script setup>
 import HeaderComponent from '@/components/main/HeaderComponent.vue'
-import AboutComponent from '../components/main/AboutComponent.vue';
+import AboutComponent from '../components/main/AboutComponent.vue'
+import TrendComponent from '../components/main/TrendComponent.vue'
 
 </script>
 <style scoped lang="scss">
