7b2c1a1e2481717bc2bf20b1e8797ba1d476e021
Nazarov-Dmitriy <Difat1@inbox.ru>
2024-06-30 22:42:32 +0300

    add main component

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -3,15 +3,24 @@
         <HeaderComponent />
         <AboutComponent />
         <ObjectiveProjectComponent />
+        <TrendComponent />
+        <WebinarComponent />
+        <NewsComponent />
         <FaqComponent />
+        <FormComponent />
+        <FooterComponent />
     </div>
 </template>
 <script setup>
 import HeaderComponent from '@/components/main/HeaderComponent.vue'
 import AboutComponent from '../components/main/AboutComponent.vue';
-// import ObjectiveProjectComponent from '../components/main/ObjectiveProjectComponent.vue';
+import ObjectiveProjectComponent from '../components/main/ObjectiveProjectComponent.vue';
 import FaqComponent from '../components/main/FaqComponent.vue';
-
+import TrendComponent from '../components/main/TrendComponent.vue';
+import WebinarComponent from '../components/main/WebinarComponent.vue';
+import NewsComponent from '../components/main/NewsComponent.vue';
+import FormComponent from '../components/main/FormComponent.vue';
+import FooterComponent from '../components/main/FooterComponent.vue';
 
 </script>
 <style scoped lang="scss">
