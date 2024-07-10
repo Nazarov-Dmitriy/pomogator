bf68b30c87f901165217c2a2fcf286598c663b70
Пользователь <olegator10205@gmail.com>
2024-07-10 15:21:02 +0300

    added become and reviews components

---
+++ src/components/about/BecomeComponent.vue
@@ -0,0 +1,66 @@
+<template>
+    <section class="become">
+        <div class="become__wrapper">
+            <h2 class="become__title">
+                Стань частью педагогического сообщества будущего и открой для себя безграничные
+                возможности для профессионального развития!
+            </h2>
+            <p class="become__subtitle">
+                Зарегистрируйся на нашем сайте и получи доступ к эксклюзивным методическим
+                материалам, онлайн-консультациям экспертов и сети единомышленников, которые меняют
+                образование.
+            </p>
+            <BtnBackground class="become__button">Регистрация</BtnBackground>
+        </div>
+    </section>
+</template>
+
+<script setup>
+import BtnBackground from '../btns/BtnBackgroud.vue'
+</script>
+
+<style scoped lang="scss">
+.become {
+    padding: 60px 80px;
+    background: $blue;
+
+    @media (max-width: $lg) {
+        padding: 40px;
+    }
+    @media (max-width: $sm) {
+        padding: 32px 16px;
+    }
+}
+.become__wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 32px;
+}
+.become__title {
+    font-weight: 600;
+    font-size: 36px;
+    line-height: 1.17;
+    text-align: center;
+    color: $white;
+    text-align: center;
+
+    @media (max-width: $lg) {
+        font-size: 32px;
+        line-height: 1.25;
+    }
+}
+.become__subtitle {
+    line-height: 1.5;
+    color: $black;
+    text-align: center;
+    max-width: 777px;
+    width: 100%;
+
+    @media (max-width: $lg) {
+        font-size: 14px;
+        line-height: 1.4;
+    }
+}
+</style>
--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -2,14 +2,18 @@
     <div class="page">
         <HeaderComponent />
         <TeacherComponent />
+        <BecomeComponent/>
          <EquipmentComponent/>
+         <ReviewsComponent/>
     </div>
 </template>
 
 <script setup>
 import HeaderComponent from '@/components/HeaderComponent.vue'
 import TeacherComponent from '../components/about/TeacherComponent.vue';
+import BecomeComponent from "../components/about/BecomeComponent.vue"
 import EquipmentComponent from "../components/about/EquipmentComponent.vue"
+import ReviewsComponent from '../components/about/ReviewsComponent.vue';
 
 </script>
 <style scoped lang="scss">
