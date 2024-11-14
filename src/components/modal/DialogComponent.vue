360c3291666fbc6cefd4eb1c82e45d19abfc48cf
unknown <oleg21082001@mail.ru>
2024-11-14 12:08:27 +0300

    изменил стили dialog component

--- src/components/modal/DialogComponent.vue
+++ src/components/modal/DialogComponent.vue
@@ -3,24 +3,21 @@
         <div
             v-show="props.show"
             v-esc="() => $emit('closeDialog')"
-            class="dialog-modal-wrapper fixed z-[999] inset-0 bg-black bg-opacity-50 flex items-center justify-center"
+            class="dialog-modal-wrapper fixed z-[999] inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4"
         >
             <div
-                class="dialog-container flex flex-col gap-4 bg-white w-[500px] p-6 rounded-lg shadow-lg relative"
+                class="dialog-container flex flex-col md:gap-16 gap-4 bg-white w-[500px] md:p-20 py-10 px-10 rounded-lg shadow-lg relative rounded-[32px]"
             >
                 <div class="absolute top-3 right-3">
                     <button @click="handleClose">X</button>
                 </div>
-                <div class="dialog-modal text-center text-xl">
-                    Вы действительно хотите удалить материал?
+                <div class="dialog-modal">
+                    <p class="text-xxl text-center">Вы точно хотите удалить материал?</p>
                 </div>
                 <div class="flex justify-center items-center gap-4">
-                    <button @click="deleteMaterial" class="bg-red-500 p-2 rounded-sm text-white">
-                        Удалить
-                    </button>
-                    <button @click="handleClose" class="bg-green-500 p-2 rounded-sm text-white">
-                        Отмена
-                    </button>
+                    <BtnBackgroud emit-name="action" @action="deleteMaterial"
+                        >Все равно удалить</BtnBackgroud
+                    >
                 </div>
             </div>
         </div>
@@ -29,6 +26,7 @@
 
 <script setup>
 import { watch } from 'vue'
+import BtnBackgroud from '../btns/BtnBackgroud.vue'
 
 const props = defineProps({
     show: {
@@ -62,6 +60,19 @@ watch(
 <style lang="scss" scoped>
 .no-scroll {
     overflow: hidden;
+}
+
+.dialog-container {
+    border: 2px solid #5b94ea;
+    background: linear-gradient(165deg, #daebff 0%, #edf5ff 100%);
+}
+
+.dialog-container {
+    :deep(.btn-bg){
+        @media(max-width: $sm){
+            width: max-content
+        }
+    }
 }
 
 .dialog-modal-wrapper {
